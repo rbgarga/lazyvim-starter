ea72cdbdf848e4e173d6b39989f121d48301d230
Renato Botelho <garga@FreeBSD.org>
2025-09-26 11:55:21 +0000

    Import some old options to review later

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,23 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.autoformat = false
+vim.opt.background = "dark"
+vim.opt.colorcolumn = "80"
+vim.opt.cursorline = true
+vim.opt.encoding = "utf8"
+vim.opt.expandtab = false
+vim.opt.fileencoding = "utf8"
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.modeline = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 8
+vim.opt.signcolumn = "yes"
+vim.opt.smartcase = true
+vim.opt.softtabstop = 8
+vim.opt.tabstop = 8
+vim.opt.undofile = true
